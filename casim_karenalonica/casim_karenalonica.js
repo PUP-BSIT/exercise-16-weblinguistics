1ff38254a8b6e82607695afeb8e95a36f60337bc
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-12-04 17:46:16 +0800

    modified codes

--- casim_karenalonica/casim_karenalonica.js
+++ casim_karenalonica/casim_karenalonica.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sortSelect = document.getElementById('sort');
-    const commentsContainer = document.getElementById('comments_container');
-    const teammateCommentsHeader = commentsContainer.querySelector('h2');
+    const commentsContainer = document.getElementById
+		('comments_container');
+    const teammateCommentsHeader = 
+		commentsContainer.querySelector('h2');
     const commentButton = document.getElementById('comment_button');
     const nameInput = document.getElementById('name');
     const commentTextInput = document.getElementById('comment_text');
@@ -9,15 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const SORT_ASCENDING = 'asc';
     const SORT_DESCENDING = 'desc';
 
-    const sortComments = (order) => {
+    function sortComments(order) {
         const comments = Array.from(commentsContainer.children);
-        comments.shift(); // Remove the header temporarily
+        comments.shift();
 
-        comments.sort((a, b) => {
+        comments.sort(function(a, b) {
             const dateA = new Date(a.getAttribute('data-date'));
             const dateB = new Date(b.getAttribute('data-date'));
 
-            return order === SORT_ASCENDING ? dateA - dateB : dateB - dateA;
+            return order === SORT_ASCENDING ? 
+				dateA - dateB : dateB - dateA;
         });
 
         while (commentsContainer.firstChild) {
@@ -26,15 +29,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
         commentsContainer.appendChild(teammateCommentsHeader);
 
-        comments.forEach((comment) => {
+        comments.forEach(function(comment) {
             commentsContainer.appendChild(comment);
         });
-    };
+    }
 
     sortComments(sortSelect.value);
 
-    sortSelect.addEventListener('change', 
-		() => sortComments(sortSelect.value));
+    sortSelect.addEventListener('change', function () {
+        sortComments(sortSelect.value);
+    });
 
     commentButton.addEventListener('click', function () {
         const name = nameInput.value.trim();
@@ -44,24 +48,24 @@ document.addEventListener('DOMContentLoaded', function () {
             const newComment = document.createElement('div');
             newComment.setAttribute('data-date', getCurrentDate());
             newComment.innerHTML = `<p><strong>Name:</strong> ${name}</p>
-				<p>${commentText}</p>`;
+                <p>${commentText}</p>`;
 
             commentsContainer.appendChild(newComment);
 
-            nameInput.value = '';
-            commentTextInput.value = '';
+            nameInput.value;
+            commentTextInput.value;
 
             sortComments(sortSelect.value);
         }
     });
 
-    const getCurrentDate = () => {
+    function getCurrentDate() {
         const now = new Date();
         const year = now.getFullYear();
         const month = now.getMonth() + 1;
         const day = now.getDate();
 
-        return `${year}-${month < 10 ? '0' + month : month}-
-			${day < 10 ? '0' + day : day}`;
-    };
+        return `${year}-${month < 10 ? '0' + month : month}
+			-${day < 10 ? '0' + day : day}`;
+    }
 });
